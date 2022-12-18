1594931e79d52d54786c588ee055aea4ae5bfd09
Asante Kelvin <kelvinowusu523@gmail.com>
2022-12-18 21:31:35 +0000

    monty

--- monty.c
+++ monty.c
@@ -1,110 +1,24 @@
 #include "monty.h"
+global_var var_global;
 /**
- * trim_and_count_spaces - remove leading and trailing spaces
- * @str: string
- * Return: number of spaces left in trimmed string
+ * main - driver function for monty program
+ * @ac: int num of arguments
+ * @av: opcode file
+ * Return: 0
  */
-int trim_and_count_spaces(char *str)
+int main(int ac, char **av)
 {
-	int count;
-	int i;
-	int len;
-
-	while (isspace(*str))
-	{
-		str++;
-	}
-
-	len = strlen(str);
-	while (len > 0 && isspace(str[len - 1]))
-	{
-		len--;
-	}
-
-	str[len] = '\0';
-
-	count = 0;
-	for (i = 0; i < len; i++)
-	{
-		if (isspace(str[i]))
-		{
-			while (isspace(str[i]))
-			{
-				i++;
-			}
-			count++;
-		}
-	}
-
-	return (count);
-}
-/**
- * check_for_comment - checks if a line is a comment
- * @line: pointer to the line being checked
- *
- * Return: 1 if line is a comment, 0 otherwise
- */
-int check_for_comment(char *line)
-{
-	int i;
-
-	if (!line)
-		return (0);
-
-	for (i = 0; line[i]; i++)
-	{
-		if (line[i] == '#')
-			return (1);
-		if (line[i] != ' ' && line[i] != '\t')
-			return (0);
-	}
-
-	return (0);
-}
-/**
- * main - main entry
- * @argc: number of arguments
- * @argv: arguments
- * Return: EXIT_SUCCESS if successful else EXIT_FAILURE
- */
-int main(int argc, char **argv)
-{
-	char *filename, *line, *instruction;
-	FILE *file;
-	unsigned int line_number;
-	size_t line_size;
 	stack_t *stack;
 
-	if (argc != 2)
-		error_usage();
-	filename = argv[1];
-	file = fopen(filename, "r");
-	validFile(filename, file);
-	line = NULL;
-	line_size = 0;
-	line_number = 0;
 	stack = NULL;
-	while (getline(&line, &line_size, file) != -1)
+	if (ac != 2)
 	{
-		line_number++;
-		if (trim_and_count_spaces(line) > 1)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-		if (check_for_comment(line) == 1)
-			continue;
-		instruction = strtok(line, " \n\t\r");
-		global_arg.arg = strtok(NULL, " \n\t\r");
-		if (instruction == NULL)
-			continue;
-		if (executeOpCodes(instruction, &stack, line_number) == 0)
-		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, instruction);
-			end_frees(stack, line, file);
-			exit(EXIT_FAILURE);
-		}
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
-	end_frees(stack, line, file);
-	return (EXIT_SUCCESS);
+
+	read_file(av[1], &stack);
+	/* recordar liberar memorias */
+	free_dlistint(stack);
+	return (0);
 }
--- monty.h
+++ monty.h
@@ -1,63 +1,91 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MONTY_H
+#define MONTY_H
+
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <ctype.h>
 
+/*--- Struct Definitions ---*/
+extern int push_arg;
 
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct stack_s
 {
-		int n;
-			struct stack_s *prev;
-				struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct instruction_s
 {
-		char *opcode;
-			void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-typedef struct var
+/**
+ * struct global_variable - opcoode and its function
+ * @file: the opcode
+ * @push_arg: function to handle the opcode
+ * @buffer: pointer to
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct global_variable
 {
-		char *arg;
-} globals;
-extern globals global_arg;
-globals global_arg;
+	FILE *file;
+	int push_arg;
+	char *buffer;
+} global_var;
 
+extern global_var var_global;
 
-void push(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
+void read_file(char *filename, stack_t **stack);
+char *parse_line(char *line, stack_t **stack, unsigned int line_number);
+typedef void (*instruct_func)(stack_t **stack, unsigned int line_number);
+instruct_func get_op_func(char *str);
+
+/*Fuxntions Monty*/
+void _pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+
+/*Functions string ascci */
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+
+/*Functions math*/
+void _sub(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
-void _mul(stack_t **stack, unsigned int line_number);
-void _stack(stack_t **stack, unsigned int line_number);
-void queue(stack_t **stack, unsigned int line_number);
-int trim_and_count_spaces(char *str);
-void free_stack(stack_t *stack);
-int is_numeric_string(char *str);
-int validFile(char *filename, FILE *file);
-instruction_t *findOpCode(char *instruction, instruction_t *instr);
-int executeOpCodes(char *instruction, stack_t **stack, unsigned int line_number);
 
-void end_frees(stack_t *stack, char *line, FILE *file);
-void free_struct(instruction_t *instructions);
-
-void error_usage();
-void error_swapping(unsigned int line_number);
-void error_unknown(unsigned int line_number, char *instruction);
-
-
+/*Utility functions*/
+void free_dlistint(stack_t *head);
+void dobfree(stack_t **stack);
+void dobfree2(stack_t **stack);
+int _isalpha(int c);
 #endif
